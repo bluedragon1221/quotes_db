506f9e9d713e05ff136414dffc9e55ce8e754253
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2023-07-25 10:57:19 -0500

    initial: add files

---
+++ functions.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+new_list() {
+    cat <<END | sqlite3 quotes.db
+	CREATE TABLE $1 (
+	    id INTERGER PRIMARY KEY,
+	    quote TEXT,
+	    who TEXT,
+	    concept TEXT
+    )
+END
+}
+
+# Quote, speaker | character (writer), concept
+add_quote() {
+    if [ "$1" == 'i' ]; then
+	quote=$(gum write --placeholder "Quote" | sed "s/'/''/g")
+	who=$(gum input --placeholder "Who said this?" | sed "s/'/''/g")
+	concept=$(gum input --placeholder "What concept does this cover?" | sed "s/'/''/g")
+
+	cat <<END | sqlite3 quotes.db
+	INSERT INTO serious (quote, who, concept) VALUES (
+	    '$quote',
+	    '$who',
+	    '$concept'
+	) 
+END
+else
+    cat <<END | sqlite3 quotes.db
+    INSERT INTO serious (quote, who, concept) VALUES ('$1', '$2', '$3') 
+END
+    fi
+}
---
+++ queries/all_serious_quotes.sql
@@ -0,0 +1,1 @@
+select quote, concept from serious;
